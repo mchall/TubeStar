4e1b2f41f67dd46ac6412a3ca00ce11dae504f57
Mike <mcsyko@gmail.com>
2015-05-28 10:31:25 +0200

    port to GitHub

---
+++ TubeStarDesktop/UI/ViewVideoDialog.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using Xceed.Wpf.Toolkit;
+
+namespace TubeStar
+{
+    /// <summary>
+    /// Interaction logic for ViewVideoDialog.xaml
+    /// </summary>
+    public partial class ViewVideoDialog : ChildWindow
+    {
+        private Video _video;
+
+        public ViewVideoDialog(Video video)
+        {
+            InitializeComponent();
+            _video = video;
+
+            Translate();
+            if (video.GetCommentsCount() == 0)
+            {
+                lblComments.Visibility = commentStack.Visibility = Visibility.Collapsed;
+            }
+            Populate(video);
+
+            string id = video.YouTubeVideoId;
+
+            if (DateTime.Now.Day == 1 && DateTime.Now.Month == 4)
+            {
+                id = "oHg5SJYRHA0";
+            }
+
+            string html = String.Format("<iframe width=\"610\" height=\"343\" src=\"http://www.youtube.com/embed/{0}?rel=0\" frameborder=\"0\"></iframe>", id);
+            this.webView.NavigateToString(html);
+        }
+
+        private void wb_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            string script = "document.body.style.overflow ='hidden'";
+            WebBrowser wb = (WebBrowser)sender;
+            wb.InvokeScript("execScript", new Object[] { script, "JavaScript" });
+        }
+
+        private void Translate()
+        {
+            Caption = EnglishStrings.ViewVideo.Translate();
+            lblComments.Text = String.Format("{0} ({1})", EnglishStrings.Comments.Translate(), _video.GetCommentsCount().ToNumberString());
+            btnOk.Content = EnglishStrings.Ok.Translate();
+        }
+
+        private void Populate(Video video)
+        {
+            List<VideoComment> orderedComments = new List<VideoComment>();
+            foreach (var item in video.Comments)
+            {
+                VideoComment likedComment = new VideoComment(item.Comment, item.Likes);
+                orderedComments.Add(likedComment);
+            }
+
+            foreach (var comment in orderedComments)
+            {
+                CommentBlock block = new CommentBlock(comment.Comment, comment.Likes);
+                commentStack.Children.Insert(0, block);
+            }
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            webView.NavigateToString("<html></html>");
+            this.DialogResult = true;
+        }
+
+        private void ChildWindow_Closed(object sender, EventArgs e)
+        {
+            webView.NavigateToString("<html></html>");
+        }
+    }
+}
---
+++ TubeStarDesktop/YouTube API/YouTubeAPI.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TubeStar
+{
+    public class YouTubeAPI
+    {
+        private const string ApiKey = "AIzaSyDL-vhmr5xpdU0d4sMDUIzQskZnC26ci24";
+
+        public static Uri GetRandomImages(string search, int max)
+        {
+            search = search.Replace(' ', ',');
+            var uriString = String.Format("https://www.googleapis.com/youtube/v3/search?part=snippet&q={0}&type=video&&maxResults={1}&fields=nextPageToken,prevPageToken,items(id(videoId))&key={2}{3}",
+                HttpHelpers.UrlEncoding(search), max, ApiKey, Settings.UseCreativeCommons ? "&videoLicense=creativeCommon" : "");
+            return new Uri(uriString);
+        }
+
+        public static Uri GetRandomImagesWithTitle(string search, int max)
+        {
+            search = search.Replace(' ', ',');
+            var uriString = String.Format("https://www.googleapis.com/youtube/v3/search?part=snippet&q={0}&type=video&&maxResults={1}&fields=nextPageToken,prevPageToken,items(id(videoId),snippet(title))&key={2}{3}",
+                HttpHelpers.UrlEncoding(search), max, ApiKey, Settings.UseCreativeCommons ? "&videoLicense=creativeCommon" : "");
+            return new Uri(uriString);
+        }
+
+        public static Uri GetRandomComments(string videoId, int max)
+        {
+            var uriString = String.Format("http://gdata.youtube.com/feeds/api/videos/{0}/comments?alt=json&max-results={1}&fields=entry(content)&key={2}",
+                videoId, max, ApiKey);
+            return new Uri(uriString);
+        }
+
+        public static Uri GetPhotoUri(string id)
+        {
+            return new Uri(String.Format("http://i.ytimg.com/vi/{0}/hqdefault.jpg", id));
+        }
+
+        public static Uri GetSmallPhotoUri(string id)
+        {
+            return new Uri(String.Format("http://i.ytimg.com/vi/{0}/default.jpg", id));
+        }
+
+        public static Uri GetLinkUri(string id)
+        {
+            return new Uri(String.Format("http://www.youtube.com/watch?v={0}", id));
+        }
+    }
+
+    [JsonObject()]
+    public class YouTubeCommentResponse
+    {
+        [JsonProperty("feed")]
+        public YouTubeCommentFeed Feed { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeCommentFeed
+    {
+        [JsonProperty("entry")]
+        public List<YouTubeCommentEntry> Entries { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeCommentEntry
+    {
+        [JsonProperty("content")]
+        public YouTubeCommentGroup Content { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeCommentGroup
+    {
+        [JsonProperty("$t")]
+        public string Comment { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeSearchResponse
+    {
+        [JsonProperty("prevPageToken")]
+        public string PreviousPageToken { get; set; }
+
+        [JsonProperty("nextPageToken")]
+        public string NextPageToken { get; set; }
+
+        [JsonProperty("items")]
+        public List<YouTubeSearchEntry> Entries { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeSearchEntry
+    {
+        [JsonProperty("id")]
+        public YouTubeSearchId Id { get; set; }
+
+        [JsonProperty("snippet")]
+        public YouTubeSearchTitle Snippet { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeSearchTitle
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    [JsonObject()]
+    public class YouTubeSearchId
+    {
+        [JsonProperty("videoId")]
+        public string VideoId { get; set; }
+    }
+}
